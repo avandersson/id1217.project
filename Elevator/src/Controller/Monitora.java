2f1b0847eed151204e65a29177ddc0e451dd3748
Ivan <ivanp@kth.se>
2012-03-07 10:17:02 +0100

    ivans first push

--- Elevator/src/Controller/Monitora.java
+++ Elevator/src/Controller/Monitora.java
@@ -4,44 +4,58 @@ import java.util.ArrayList;
 
 public class Monitora {
 	ArrayList<String> listOfTasks = new ArrayList<String>();
-	//String listOfTasks[] = new String[5];
+	// String listOfTasks[] = new String[5];
 	private int direction;
-	
-	public synchronized void setTask(String command, int number){
-		
-		if(command.equals("p")){
+	private Object sync = new Object();
+
+	public void setTask(String command, int number) {
+
+		if (command.equals("p")) {
 			/*
 			 * Elevator button pressed
 			 */
-			listOfTasks.add(new String("p "+ number));
-			notify();
-			
-		}
-		
-	}
-	
-	
-	public synchronized String getTask(){
-		if(listOfTasks.size() == 0){
-			try {
-				wait();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
+			synchronized (listOfTasks) {
+				listOfTasks.add(new String("p " + number));
+				notify();
+
 			}
+
 		}
-		String ret = listOfTasks.get(0);
-		listOfTasks.remove(0);
-		return ret;
+
 	}
 
-
-	public synchronized int getDirection() {
-		return direction;
+	public String getTask() {
+		synchronized (listOfTasks) {
+			if (listOfTasks.size() == 0) {
+				try {
+					wait();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			String ret = listOfTasks.get(0);
+			listOfTasks.remove(0);
+			return ret;
+		}
 	}
 
+	public int getDirection() {
+		synchronized (sync) {
+			return direction;		
+		}
+	
+	}
 
-	public synchronized void setDirection(int direction) {
-		this.direction = direction;
+	public void setDirection(int direction) {
+		synchronized (sync) {
+			this.direction = direction;	
+		}
+	}
+
+	public int getSizeOfTaks() {
+		synchronized (listOfTasks) {
+			return listOfTasks.size();
+		}
 	}
 
 }

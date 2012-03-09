3ae2ddada6ad0d2849635b36824b3794c209d08c
Ivan <ivanp@kth.se>
2012-03-09 11:28:39 +0100

    Added comments

--- Elevator/src/Controller/Task.java
+++ Elevator/src/Controller/Task.java
@@ -1,5 +1,10 @@
 package Controller;
 
+/**
+ * Task object. Contains the necessary information about each task
+ * 
+ * @author Alfred Andersson, Ivan Pedersen
+ */
 public class Task {
 	private String command;
 	private int floor, direction;
@@ -9,15 +14,23 @@ public class Task {
 		this.floor = floor;
 		this.direction = direction;
 	}
-	
+	/**
+	 * @return command The command of this task
+	 */
 	public String getCommand() {
 		return command;
 	}
 
+	/**
+	 * @return floor The floor requested by this task
+	 */
 	public int getFloor() {
 		return floor;
 	}
 
+	/**
+	 * @return direction The direction of this task
+	 */
 	public int getDirection() {
 		return direction;
 	}
